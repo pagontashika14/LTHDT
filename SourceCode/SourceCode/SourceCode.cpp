b00a732853adc4d587432770be3df8542f8893aa
Duong Tuan Dat <pagontashika13@gmail.com>
2015-10-14 19:44:28 +0700

    Added end tested Sqlite3

--- SourceCode/SourceCode/SourceCode.cpp
+++ SourceCode/SourceCode/SourceCode.cpp
@@ -2,10 +2,26 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+#include "sqlite3.h"
 
 int main()
 {
+	sqlite3 *db;
+	char *zErrMsg = 0;
+	int rc;
+
+	rc = sqlite3_open("8.db", &db);
+
+	if (rc) {
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+		exit(0);
+	}
+	else {
+		fprintf(stderr, "Opened database successfully\n");
+	}
+	sqlite3_close(db);
+	system("pause");
     return 0;
 }
 
